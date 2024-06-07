b3d11b7959b8b8233ce23ada87c240263b4113d3
DerrickLegacy <ahaabwederrick67@gmail.com>
2024-06-07 23:50:39 +0300

    first react custom hook

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
+import useToggle from './custom_hook/useToggle';
+
 function App() {
+  const [isVisible, toggle] = useToggle()
+  const [isVisible2, toggle2] = useToggle()
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <button onClick={toggle} >
+        {isVisible ? "Hide" : "Show"}
+      </button> 
+      {isVisible && <h1>Hidden Text</h1>}
+      <br/>
+
+      <button onClick={toggle2} >
+        {isVisible2 ? "Hide2" : "Show2"}
+      </button>
+      {isVisible2 && <h1>Second Hidden Text</h1>}
+
     </div>
   );
 }
---
+++ src/custom_hook/useToggle.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+
+
+function useToggle(initialValue = false) {
+    const [state, setState] = useState(initialValue);
+
+    function toggle(){
+        setState((prev)=>!prev)
+    }
+    return [state, toggle]
+}
+
+export default useToggle
